a5a8b09d36212cac2efbf80f29753096306a268a
Arshadm <arshadsameemdeem@gmail.com>
2021-05-26 17:31:21 +0530

    added loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, {useEffect, useState} from 'react'
 import List from './components/List'
-import Pagination from './components/Pagination'
+import axios from 'axios'
+//import Pagination from './components/Pagination'
 
 function App() {
+  const url = 'https://api.github.com/search/repositories?q=created:>'+getCurrentDate()+'&sort=stars&order=desc'
+  const [repos, setRepo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setpostPerPage] = useState(10);
+
+  function getCurrentDate(){
+    var date = new Date();
+    date.setDate(date.getDate() -30);
+    return date.toISOString().split('T')[0];
+  }
+
+  useEffect(() => {
+    const fetchdata = async () => {
+        setLoading(true);
+        const responce = await axios.get(url);
+        setRepo(responce.data.items);
+        setLoading(false);
+    }
+
+    console.log(repos);
+    fetchdata();
+  }, [url])
 
   return (
       <div className="container">
-        <List />
+        <List repos={repos} loading={loading}/>
       </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,48 +1,38 @@
 import './List.css'
 import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import Pagination from './Pagination'
+//import axios from 'axios'
+//import Pagination from './Pagination'
 
-const List = () => {
-    const url = 'https://api.github.com/search/repositories?q=created:>'+getCurrentDate()+'&sort=stars&order=desc'
-    const [persons, setPersons] = useState([]);
-
-    function getCurrentDate(){
-        var date = new Date();
-        date.setDate(date.getDate() -30);
-        return date.toISOString().split('T')[0];
-    }
-
-    useEffect(() => {
-        axios.get(url)
-        .then(res => {
-            setPersons(res.data.items);
-        })
-    }, [url])
-
+const List = ({repos, loading}) => {
+    if (loading){
         return (
-            <body>
-                <div className='flex-container'>
-                    <h1>Github Most Starred Repos</h1>
-                    {persons.map(person => (
-                            <ul>
-                                <li className='list'>
-                                    <img className='image' src={person.owner.avatar_url} />
-                                    <h2 className='header'>{person.full_name}</h2>
-                                    <p className='item3'>{person.description}</p>
-                                    <div className='clips'>
-                                        <p className="item4">Stars: {person.stargazers_count}</p>
-                                        <p className="item5">Issues: {person.open_issues_count}</p>
-                                        <p id="minContainer3">Submitted by {person.owner.login}</p>
-                                    </div>
-                                </li>
-                            </ul>
-                    ))}
-                </div>
-                
-            </body>
+            <h1>Loading...</h1>
         )
     }
+    
+    return (
+        <body>
+            <div className='flex-container'>
+                <h1>Github Most Starred Repos</h1>
+                {repos.map(repos => (
+                        <ul>
+                            <li className='list'>
+                                <img className='image' src={repos.owner.avatar_url} />
+                                <h2 className='header'>{repos.full_name}</h2>
+                                <p className='item3'>{repos.description}</p>
+                                <div className='clips'>
+                                    <p className="item4">Stars: {repos.stargazers_count}</p>
+                                    <p className="item5">Issues: {repos.open_issues_count}</p>
+                                    <p id="minContainer3">Submitted by {repos.owner.login}</p>
+                                </div>
+                            </li>
+                        </ul>
+                ))}
+            </div>
+            
+        </body>
+    )
+}
 
 export default List
 
